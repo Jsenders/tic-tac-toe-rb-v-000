ce01b8370b54280f824ef08aba9b7ed048df6c64
Joseph Senders <jasenders@gmail.com>
2018-05-28 14:27:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,8 +39,16 @@ end
 def turn_count(board)
 end
 
-def current_player
-end
+def current_player(board)
+    #if turn_count(board) %  == 0
+      num = turn_count(board)
+      if num % 2 == 0
+        return "X"
+      else
+        return "O"
+      #puts "divisible by 2"
+      end
+  end
 
 def won?(board)
 WIN_COMBINATIONS.each do |array|
