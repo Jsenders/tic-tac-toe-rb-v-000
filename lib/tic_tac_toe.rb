b5bf94b00615bf7cd4b6e149cb27291868741863
Joseph Senders <jasenders@gmail.com>
2018-05-28 15:29:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -89,9 +89,7 @@ def play(board)
     turn(board)
   end
   if winner(board)
-    puts "Congratulations X!"
-  elsif winner(board)
-    puts "Congratulations O!"
+    puts "Congratulations X!" || puts "Congratulations O!"
   elsif draw?(board)
     puts "Cat's Game!"
   else
